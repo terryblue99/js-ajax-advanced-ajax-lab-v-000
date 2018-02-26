8b6b59b16543964a2e2f94d366f911b5e25c52af
Terry Blue <terryblue@comcast.net>
2018-02-26 22:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ function getRepositories() {
 
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
-  // get the innerHTML of the template script tag to compile into a template 
-  // function that the JSON response is passed into
+  // We get the innerHTML of our template script tag to compile into a template 
+  // function that we pass our JSON response into. 
+  // Since we are giving the template function the entire collection of 
+  // repository objects, adding new fields to our template is as easy as 
+  // creating the markup
   const src = document.getElementById("repository-template").innerHTML
   const template = Handlebars.compile(src)
   const repoList = template(repos)
