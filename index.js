9e5b8beebbe0a7e00383a358c3fe86303495cae1
Terry Blue <terryblue@comcast.net>
2018-02-26 22:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// register the partial with Handlebars when the page loads
 document.addEventListener("DOMContentLoaded", function(event) {
   Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
 });
